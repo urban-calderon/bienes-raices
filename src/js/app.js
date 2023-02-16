57f4f858e1caf895703c1416a7e1b17cc393cfe5
urbancalderon3 <urbancalderon3@gmail.com>
2023-02-15 21:07:44 -0600

    ADDED: create responsive navigation and dark mode button functionality

---
+++ src/js/app.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+    eventListeners()
+    darkMode()
+})
+
+function darkMode() {
+
+    // Leer las preferencias del usuario en el sistema operativo para el color del tema
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
+
+    if(prefiereDarkMode.matches) {
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if(prefiereDarkMode.matches) {
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    })
+
+    const botonDarkMode = document.querySelector('.dark-mode-boton')
+
+    botonDarkMode.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode')
+    })
+}
+
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu')
+    mobileMenu.addEventListener('click', navegacionResponsive)
+}
+
+function navegacionResponsive() {
+    const navegacion = document.querySelector('.navegacion')
+
+    // Sintaxis básica con una condición
+    /* if(navegacion.classList.contains('mostrar')) {
+        navegacion.classList.remove('mostrar')
+    } else {
+        navegacion.classList.add('mostrar')
+    } */
+
+    // Otra sintaxis similar a la condición:
+    navegacion.classList.toggle('mostrar')
+
+}
