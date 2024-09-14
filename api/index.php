1cb0d7564d6dea9dff3a8e7eb524b7897f2a4079
Urbano Calderón Jiménez <103982150+urban-calderon@users.noreply.github.com>
2024-09-13 18:34:10 -0600

    actualizacion sintaxis

--- api/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-  * Here is the serverless function entry
-
-  * for deployment with Vercel.
-
-*/
-
-require __DIR__ . './index.php';
--- includes/funciones.php
+++ includes/funciones.php
@@ -3,5 +3,5 @@
 require 'app.php';
 
 function incluirTemplate(string $nombre, bool $inicio = false ) {
-    include TEMPLATES_URL . "/${nombre}.php";
+    include TEMPLATES_URL . "/{$nombre}.php";
 }
